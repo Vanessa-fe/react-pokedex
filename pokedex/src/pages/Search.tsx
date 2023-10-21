5453f92441a00bfba2017143a8e5042978f2afbc
vanessa-fe <van3ssacod3@gmail.com>
2023-10-21 19:36:02 +0200

    début scss carte pokemon

--- pokedex/src/pages/Search.tsx
+++ pokedex/src/pages/Search.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Wrapper from '../sections/Wrapper'
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { getInitialPokemonData } from '../app/reducers/getInitialPokemonData';
+import { getPokemonData } from '../app/reducers/getPokemonData';
+import PokemonCardGrid from '../components/PokemonCardGrid';
 
 function Search() {
-  return (
-    <div>
-      Search
-    </div>
-  )
+  const dispatch = useAppDispatch();
+  const { allPokemon, randomPokemons } = useAppSelector(({ pokemon }) => pokemon);
+  useEffect(() => {
+    dispatch(getInitialPokemonData());
+  }, [dispatch]);
+  
+  useEffect(() => {
+    if (allPokemon) {
+      const clonedPokemons = [...allPokemon];
+      const randomPokemonsId = clonedPokemons
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 20);
+      dispatch(getPokemonData(randomPokemonsId));
+    }
+  }, [allPokemon, dispatch]);
+
+  return <>
+  <div className="search">
+    <input type="text" name="" id="" />
+    <PokemonCardGrid pokemons={randomPokemons!}/>
+  </div>
+  </>;
 }
 
 export default Wrapper(Search);
--- pokedex/src/utils/Types.ts
+++ pokedex/src/utils/Types.ts
@@ -1,1 +1,32 @@
 export interface AppTypeInitiaState {}
+
+export interface PokemonTypeInitialState {
+    allPokemon: undefined | genericPokemonType[];
+    randomPokemons: undefined | generatedPokemonType[];
+}
+
+export interface genericPokemonType {
+    name: string;
+    url: string;
+}
+
+export interface generatedPokemonType {
+    name: string;
+    id: number;
+    image: string;
+    types: pokemonTypeInterface[]
+}
+
+export interface pokemonTypeInterface {
+    [key: string]: {
+        image: string;
+        resistance: string[];
+        strength: string[];
+        weakness: string[];
+        vulnerable: string[];
+    };
+}
+
+export interface userPokemonsType extends generatedPokemonType {
+    firebaseId?: string;
+}
